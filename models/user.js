f17195e22375b4f9bdbf8e6f56ba3406233e567d
vinay kumar maurya <113638115+vinne01@users.noreply.github.com>
2025-01-23 16:55:09 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,11 +2,7 @@ const mongoose =require('mongoose');
 
 // mongoose.connect("mongodb://127.0.0.1:27017/posthand");
 mongoose.connect("mongodb+srv://vinne0563:mLUi7raEfVS7VMZS@vinaybhai.hexp2.mongodb.net/posthand");
-// mongoose.connect(process.env.MONGO_URI)
-//     .then(() => console.log('MongoDB connected'))
-//     .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-// console.log(`the db is connect with ${mongoose.connection.host}`);
 
 const userSchema =mongoose.Schema({
     username:String,
